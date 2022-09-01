aa40147e351e1ae48cc879939c24591628db393c
Bellie Joe <jandusayjoe14@gmail.com>
2022-09-01 15:46:54 +0800

    Testing the routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return "Blog CRUD application";
     // return view('welcome');
 });
