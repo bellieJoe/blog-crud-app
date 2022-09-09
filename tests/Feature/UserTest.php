99cfca6d101521fab400a66f105802ce1714f473
Bellie Joe <jandusayjoe14@gmail.com>
2022-09-09 14:40:26 +0800

    Added test cases for User Routes

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -5,27 +5,15 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    // use RefreshDatabase;
-
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_sample()
-    {
-        $response = $this->get('/');
-
-        User::factory()->make();
-
-        $response->assertStatus(200);
-    }
+    use RefreshDatabase;
 
     public function test_sign_up_view(){
+        
 
         $response = $this->get('/signup');
 
@@ -33,15 +21,66 @@ class UserTest extends TestCase
 
     }
 
-    public function test_user_register(){
+    public function test_user_register_should_fail(){
+
+        $user = User::factory()->make();
+
         $response = $this->post('/user/register', [
-            'email' => 'test.email@densss.com',
-            'fullname' => 'Test User Name',
+            'email' => $user->email,
+            'fullname' => $user->fullname,
+            'password' => 'passwors',
+            'password_confirmation' => 'passwords'
+        ]);
+        
+        $response->assertSessionHasErrors();
+    }
+
+    public function test_user_register_should_succeed(){
+        $user = User::factory()->make();
+        
+        $response = $this->post('/user/register', [
+            'email' => $user->email,
+            'fullname' => $user->fullname,
             'password' => 'password',
             'password_confirmation' => 'password'
         ]);
 
-        $response->assertStatus(302);
+        $response->assertRedirect('/blog');
+        $response->assertSessionHasNoErrors();
 
     }
+
+    public function test_user_logout(){
+        $response = $this->get('/user/logout');
+
+        $response->assertRedirect('');
+        $response->assertSessionHasNoErrors();
+        $this->assertGuest();
+    }
+
+    public function test_user_login_should_fail(){
+
+        $user = User::factory()->create();
+
+        $response = $this->post('/user/login', [
+            'email' => $user->email,
+            'password' => 'wrong pass',
+        ]);
+
+        $response->assertSessionHasErrors();
+    }
+
+    public function test_user_login_should_succeed(){
+        $user = User::factory()->create();
+
+        $response = $this->post('/user/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertSessionHasNoErrors();
+        $this->assertAuthenticated();
+    }
+
+    
 }
